9da11f86cd78d1bd819db06cd6fb09b609965f4e
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-12 12:13:23 +0100

    added fake dashboard projects

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import Heading from '@/components/Heading.vue';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -17,19 +20,22 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+            <div class="p-4 flex flex-col gap-4">
+                <Heading title="Projects" description="These are your latest projects" />
+                <div class="flex flex-col gap-2">
+                    <div v-for="index in [...Array(3).keys()]" :key="index" class="flex justify-between  rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800">
+                        <div class="text-sm font-medium px-4 py-2">Project name</div>
+                        <div class="flex gap-2">
+                            <Button variant="default">Edit</Button>
+                            <Button variant="destructive">Delete</Button>
+                        </div>
+                    </div>
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="grid gap-1">
+                    <Label for="name">Search projects</Label>
+                    <Input id="search" class="mt-1 block w-full" placeholder="Project name" />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
-                </div>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <PlaceholderPattern />
+                <Button>Add new</Button>
             </div>
         </div>
     </AppLayout>

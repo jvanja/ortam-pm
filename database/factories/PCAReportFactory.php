c544a7664c8e59fd4aca66f6b0b8948254449acf
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-14 14:25:17 +0100

    added seeder

---
+++ database/factories/PCAReportFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PCAReportFactory extends Factory {
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array {
+    return [
+      'name' => $this->faker->company(),
+      'occupation_of_the_property' => 'Residential',
+      'total_site_area' => $this->faker->numberBetween(2000, 5000), // m²
+      'surface_area_of_the_building' => $this->faker->numberBetween(500, 1000), // m²
+      'occupation_of_the_building' => $this->faker->randomFloat(2, 10, 50), // percentage
+      'year_of_construction' => $this->faker->year(),
+      'structure' => $this->faker->randomElement(['Reinforced concrete', 'Steel frame', 'Wood frame']),
+      'fondation' => $this->faker->randomElement(['Reinforced concrete', 'Piled foundation', 'Slab-on-grade']),
+      'building' => $this->faker->randomElement(['Bricks', 'Metal siding', 'Vinyl siding']),
+      'numbers_of_floors' => $this->faker->numberBetween(1, 10),
+      'basement' => $this->faker->boolean(30), // 30% chance of having a basement
+      'residential_units' => $this->faker->numberBetween(10, 100),
+      'non_residential_units' => $this->faker->numberBetween(0, 10),
+    ];
+  }
+}

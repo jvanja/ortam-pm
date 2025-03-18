1c382cb2db65e8d9b0a6c054b60ce61e68836f05
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-18 14:50:51 +0100

    added sumary

---
+++ resources/js/components/PCAReport/Summary.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+
+import { type p_c_a_reportsEntity } from '@/types/DatabaseModels';
+import { reactive, watch } from 'vue';
+
+const props = defineProps<{ form: p_c_a_reportsEntity }>();
+
+const emit = defineEmits(['update:form']);
+
+const localForm = reactive({ ...props.form });
+
+// Watch for changes and emit updates
+watch(
+  localForm,
+  (newVal) => {
+    emit('update:form', { ...newVal });
+  },
+  { deep: true },
+);
+</script>
+
+<template>
+  <div>
+    <Label htmlFor="o-prop">Occupation of the property</Label>
+    <Input id="o-prop" v-model="localForm.occupation_of_the_property"></Input>
+
+    <Label htmlFor="name">Total Site area:</Label>
+    <Input v-model="localForm.total_site_area"></Input>
+
+    <Label htmlFor="name">Surface area of the building (Approx.)</Label>
+    <Input v-model="localForm.surface_area_of_the_building"></Input>
+
+    <Label htmlFor="name">Occupation of the building</Label>
+    <Input v-model="localForm.occupation_of_the_building"></Input>
+
+    <Label htmlFor="name">Year of construction</Label>
+    <Input v-model="localForm.year_of_construction"></Input>
+
+    <Label htmlFor="name">Building</Label>
+    <Input v-model="localForm.building"></Input>
+
+    <Label htmlFor="name">Number of floors:</Label>
+    <Input v-model="localForm.numbers_of_floors"></Input>
+
+    <Label htmlFor="basement">Basement</Label>
+    <RadioGroup id="basement" :default-value="localForm.basement.toString()" :orientation="'vertical'">
+      <div class="flex items-center space-x-2">
+        <RadioGroupItem id="r1" value="true" />
+        <Label for="r1">Yes</Label>
+      </div>
+      <div class="flex items-center space-x-2">
+        <RadioGroupItem id="r2" value="false" />
+        <Label for="r2">No</Label>
+      </div>
+    </RadioGroup>
+
+    <Label htmlFor="name">Non-residential units</Label>
+    <Input v-model="localForm.non_residential_units"></Input>
+
+    <Label htmlFor="name">Residential units</Label>
+    <Input v-model="localForm.residential_units"></Input>
+  </div>
+</template>
--- resources/js/pages/pca_reports/Show.vue
+++ resources/js/pages/pca_reports/Show.vue
@@ -3,10 +3,9 @@ import { type BreadcrumbItem } from '@/types';
 import { type p_c_a_reportsEntity } from '@/types/DatabaseModels';
 
 import Interior from '@/components/PCAReport/Interior.vue';
+import Summary from '@/components/PCAReport/Summary.vue';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Toaster } from '@/components/ui/sonner';
 import { Tabs, TabsContent, TabsTrigger, TabsList } from '@/components/ui/tabs';
 import AppLayout from '@/layouts/AppLayout.vue';
@@ -59,8 +58,8 @@ const tabsMenu = [
       <div class="flex flex-col gap-4 p-4">
         <Card>
           <CardHeader>
-            <CardTitle>{{ props.pca_report.name }}</CardTitle>
-            <CardDescription>{{ props.pca_report.year_of_construction }}</CardDescription>
+            <CardTitle>{{ pca_report.name }}</CardTitle>
+            <CardDescription>{{ pca_report.year_of_construction }}</CardDescription>
           </CardHeader>
           <CardContent>
             <form @submit.prevent="submit" class="space-y-6">
@@ -68,15 +67,8 @@ const tabsMenu = [
                 <TabsList aria-label="tabs example">
                   <TabsTrigger v-for="tab in tabsMenu" :value="tab.tab" :key="tab.tab">{{tab.title}}</TabsTrigger>
                 </TabsList>
-                <TabsContent value="summary">
-                  <div class="grid w-full items-center gap-4">
-                    <div class="flex flex-col space-y-1.5">
-                      <Label htmlFor="name">Occupation of the property</Label>
-                      <Input v-model="form.occupation_of_the_property" id="name" :placeholder="props.pca_report.occupation_of_the_property" />
-                    </div>
-                  </div>
-                </TabsContent>
-                <TabsContent value="interior"><Interior :form="form" @update:form="handleFormUpdate" /></TabsContent>
+                <TabsContent value="summary"><Summary :form="pca_report" @update:form="handleFormUpdate" /></TabsContent>
+                <TabsContent value="interior"><Interior :form="pca_report" @update:form="handleFormUpdate" /></TabsContent>
               </Tabs>
               <div class="flex w-full justify-end">
                 <Button :disabled="form.processing">Save</Button>

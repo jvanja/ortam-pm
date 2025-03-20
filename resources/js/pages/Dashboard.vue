61dedf50dfc4a2a625abea6039a27e0c6280d4f8
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-20 16:34:05 +0100

    added new project compoent

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Heading from '@/components/Heading.vue';
+import NewProject from '@/components/project/NewProject.vue';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -11,6 +12,7 @@ import { Head } from '@inertiajs/vue3';
 const props = defineProps<{
   projects: projectsEntity[];
 }>();
+console.log(props.projects);
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
@@ -18,32 +20,32 @@ const breadcrumbs: BreadcrumbItem[] = [
     href: '/dashboard',
   },
 ];
-
-console.log(props.projects);
 </script>
 
 <template>
   <Head title="Dashboard" />
 
   <AppLayout :breadcrumbs="breadcrumbs">
-    <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-      <div class="flex flex-col gap-4 p-4">
-        <Heading title="Projects" description="These are your latest projects" />
-        <div class="flex flex-col gap-2">
-          <div v-for="project in projects" :key="project.id" class="flex justify-between rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800">
-            <div class="px-4 py-2 text-sm font-medium">{{ project.project_type }}</div>
-            <div class="flex gap-2">
-              <Button variant="default"><a :href="`/projects/${project.id}`">Edit</a></Button>
-              <Button variant="destructive">Delete</Button>
-            </div>
+    <div v-if="projects.length > 10" class="flex flex-col gap-4 p-8">
+      <Heading title="Projects" description="These are your latest projects" />
+      <div class="flex flex-col gap-2">
+        <div v-for="project in projects" :key="project.id" class="flex justify-between rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800">
+          <div class="px-4 py-2 text-sm font-medium">{{ project.project_type }}</div>
+          <div class="flex gap-2">
+            <Button variant="default"><a :href="`/projects/${project.id}`">Edit</a></Button>
+            <Button variant="destructive">Delete</Button>
           </div>
         </div>
-        <div class="grid gap-1">
-          <Label for="name">Search projects</Label>
-          <Input id="search" class="mt-1 block w-full" placeholder="Project name" />
-        </div>
-        <Button>Add new</Button>
       </div>
+      <div class="grid gap-1">
+        <Label for="name">Search projects</Label>
+        <Input id="search" class="mt-1 block w-full" placeholder="Project name" />
+      </div>
+      <Button>Add new</Button>
+    </div>
+    <div v-else class="flex flex-col gap-4 p-8">
+      Let's create some
+      <NewProject></NewProject>
     </div>
   </AppLayout>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -8,25 +8,25 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-     if (Auth::check()) {
-        return redirect('/dashboard');
-    }
-    return Inertia::render('Welcome');
+  if (Auth::check()) {
+    return redirect('/dashboard');
+  }
+  return Inertia::render('Welcome');
 })->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'show'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/translations/{locale}', function ($locale) {
-    $path = base_path("lang/{$locale}.json");
-    if (!File::exists($path)) {
-        abort(404);
-    }
-    return response()->json(json_decode(File::get($path), true));
+  $path = base_path("lang/{$locale}.json");
+  if (!File::exists($path)) {
+    abort(404);
+  }
+  return response()->json(json_decode(File::get($path), true));
 });
 
 Route::get('/set-locale/{locale}', function ($locale, Request $request) {
-    $request->session()->put('locale', $locale);
-    return redirect()->back();
+  $request->session()->put('locale', $locale);
+  return redirect()->back();
 });
 
 require base_path('routes/projects.php');

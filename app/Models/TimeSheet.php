62eb3c7c7a05910c8592a0c495e8287dda88cfbf
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-21 09:39:24 +0100

    added hasfactories

--- app/Models/TimeSheet.php
+++ app/Models/TimeSheet.php
@@ -2,35 +2,37 @@
 
 namespace App\Models;
 
-use App\Enums\Task;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\Task;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TimeSheet extends Model {
-    use HasUuids;
+  use HasUuids;
+  use HasFactory;
 
-    protected $fillable = [
-        'task_performed',
-        'project_id',
-        'worked_duration',
-    ];
+  protected $fillable = [
+    'task_performed',
+    'project_id',
+    'worked_duration',
+  ];
 
-    protected $casts = [
-        'id' => 'string',
-        'task_performed' => Task::class,
-        'worked_duration' => 'float',
-    ];
+  protected $casts = [
+    'id' => 'string',
+    'task_performed' => Task::class,
+    'worked_duration' => 'float',
+  ];
 
-    public function user(): BelongsTo {
-        return $this->belongsTo(User::class);
-    }
+  public function user(): BelongsTo {
+    return $this->belongsTo(User::class);
+  }
 
-    public function project(): BelongsTo {
-        return $this->belongsTo(Project::class);
-    }
+  public function project(): BelongsTo {
+    return $this->belongsTo(Project::class);
+  }
 
-    public function getTotalHours(): float {
-        return $this->worked_duration;
-    }
+  public function getTotalHours(): float {
+    return $this->worked_duration;
+  }
 }

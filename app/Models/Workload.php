e46d18c21fe573cefbb3cbb779a4f44197d90619
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-21 09:45:47 +0100

    moved workload to the controller

--- app/Models/Workload.php
+++ app/Models/Workload.php
@@ -28,13 +28,4 @@ class Workload extends Model {
   public function user(): BelongsTo {
     return $this->belongsTo(User::class);
   }
-
-  // public function visualizeWorkload(): GanttChart
-  // {
-  //     // TODO: Implement visualizeWorkload() method.
-  // }
-
-  public function updateWorkload(Project $project): void {
-    // TODO: Implement updateWorkload() method.
-  }
 }

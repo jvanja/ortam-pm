ad108056d625476f388bef23ff9a95a37be69fb8
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-23 12:15:45 +0100

    added index clients

--- resources/js/pages/clients/Index.vue
+++ resources/js/pages/clients/Index.vue
@@ -5,45 +5,45 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { type projectsEntity } from '@/types/DatabaseModels';
+import { type clientsEntity } from '@/types/DatabaseModels';
 import { Head } from '@inertiajs/vue3';
 
 const props = defineProps<{
-    projects: projectsEntity[];
+  clients: clientsEntity[];
 }>();
 
 const breadcrumbs: BreadcrumbItem[] = [
-    {
-        title: 'Projects',
-        href: '/projects',
-    },
+  {
+    title: 'Clients',
+    href: '/clients',
+  },
 ];
 
-console.log(props.projects);
+console.log(props.clients);
 </script>
 
 <template>
-    <Head title="Dashboard" />
+  <Head title="Dashboard" />
 
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="flex flex-col gap-4 p-4">
-                <Heading title="Projects" description="These are your latest projects" />
-                <div class="flex flex-col gap-2">
-                    <div v-for="project in projects" :key="project.id" class="flex justify-between rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800">
-                        <div class="px-4 py-2 text-sm font-medium">{{ project.project_type }}</div>
-                        <div class="flex gap-2">
-                            <Button variant="default"><a :href="`/projects/${project.id}`">Edit</a></Button>
-                            <Button variant="destructive">Delete</Button>
-                        </div>
-                    </div>
-                </div>
-                <div class="grid gap-1">
-                    <Label for="name">Search projects</Label>
-                    <Input id="search" class="mt-1 block w-full" placeholder="Project name" />
-                </div>
-                <Button>Add new</Button>
+  <AppLayout :breadcrumbs="breadcrumbs">
+    <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+      <div class="flex flex-col gap-4 p-4">
+        <Heading title="Clients" description="These are your latest clients" />
+        <div class="flex flex-col gap-2">
+          <div v-for="client in clients" :key="client.id" class="flex justify-between rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800">
+            <div class="px-4 py-2 text-sm font-medium">{{ client.company_name }}</div>
+            <div class="flex gap-2">
+              <Button variant="default"><a :href="`/projects/${client.id}`">Edit</a></Button>
+              <Button variant="destructive">Delete</Button>
             </div>
+          </div>
         </div>
-    </AppLayout>
+        <div class="grid gap-1">
+          <Label for="name">Search clients</Label>
+          <Input id="search" class="mt-1 block w-full" placeholder="Client name" />
+        </div>
+        <Button>Add new</Button>
+      </div>
+    </div>
+  </AppLayout>
 </template>

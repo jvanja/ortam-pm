cb2d92a569450095a5935b0d1fdb4c553b78154d
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-24 09:09:27 +0100

    format

--- database/factories/QuoteFactory.php
+++ database/factories/QuoteFactory.php
@@ -10,23 +10,21 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quote>
  */
-class QuoteFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'project_type' => fake()->jobTitle(),
-            'quote_language' => fake()->randomElement(Language::cases()),
-            'project_address' => fake()->address(),
-            'budget' => fake()->randomFloat(2, 1000, 10000),
-            'sales_representative_name' => fake()->name(),
-            'quote_status' => fake()->randomElement(QuoteStatus::cases()),
-            'project_id' => Project::factory(),
-        ];
-    }
+class QuoteFactory extends Factory {
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array {
+    return [
+      'project_type' => fake()->jobTitle(),
+      'quote_language' => fake()->randomElement(Language::cases()),
+      'project_address' => fake()->address(),
+      'budget' => fake()->randomFloat(2, 1000, 10000),
+      'sales_representative_name' => fake()->name(),
+      'quote_status' => fake()->randomElement(QuoteStatus::cases()),
+      'project_id' => Project::factory(),
+    ];
+  }
 }

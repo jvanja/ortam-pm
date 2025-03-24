993cb4fb7efd7f563e39944de7f19acec653fe21
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-24 08:56:57 +0100

    pulled organization name

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { usePage } from '@inertiajs/vue3'
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+
+const page = usePage()
+const organization = computed(() => page.props.organization )
 </script>
 
 <template>
@@ -7,6 +12,6 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
     <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
   </div>
   <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate font-semibold leading-none">Miki</span>
+    <span class="mb-0.5 truncate font-semibold leading-none">{{ organization }}</span>
   </div>
 </template>

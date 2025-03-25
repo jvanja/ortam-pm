e03015f732be5c2e4ebab96118005e9e9991c273
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-25 15:03:03 +0100

    removed projects routes

--- routes/projects.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\ProjectController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-  Route::resource('projects', ProjectController::class); //->except(['store']);
-});

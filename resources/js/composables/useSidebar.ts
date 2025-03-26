68fe4026fe06c8c71c4f6678aee816b9c8b6d10b
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-26 15:47:58 +0100

    format

--- resources/js/composables/useSidebar.ts
+++ resources/js/composables/useSidebar.ts
@@ -1,34 +1,34 @@
-import { ref } from 'vue';
 import { type NavItem } from '@/types';
-import { Contact, Folder, Clipboard, LayoutGrid } from 'lucide-vue-next';
+import { Clipboard, Contact, Folder, LayoutGrid } from 'lucide-vue-next';
+import { ref } from 'vue';
 
 const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'PCA Reports',
-        href: '/pca-reports',
-        icon: Clipboard,
-    },
-    {
-        title: 'Projects',
-        href: '/projects',
-        icon: Folder,
-    },
-    {
-        title: 'Clients',
-        href: '/clients',
-        icon: Contact,
-    },
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+    icon: LayoutGrid,
+  },
+  {
+    title: 'PCA Reports',
+    href: '/pca-reports',
+    icon: Clipboard,
+  },
+  {
+    title: 'Projects',
+    href: '/projects',
+    icon: Folder,
+  },
+  {
+    title: 'Clients',
+    href: '/clients',
+    icon: Contact,
+  },
 ];
 const sidebarMenu = ref(mainNavItems);
 
 export default function useSidebar() {
   const setSidebarMenu = (newMenu?: NavItem[]) => {
-    if(newMenu) sidebarMenu.value = newMenu;
+    if (newMenu) sidebarMenu.value = newMenu;
     else sidebarMenu.value = mainNavItems;
   };
 

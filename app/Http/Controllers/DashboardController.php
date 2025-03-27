1b61161b0bc46a50df35f172c0677e784c23aaa2
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-03-27 10:01:00 +0100

    added separate dashboards

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Project;
 use Inertia\Inertia;
 
 class DashboardController extends Controller {
-    public function show() {
-        $latestProjects = Project::latest()->take(3)->get();
+  public function show() {
 
-        return Inertia::render('Dashboard', [
-            'projects' => $latestProjects,
-        ]);
+    $user = Auth::user();
+
+    if ($user->hasRole(['superadmin', 'admin'])) {
+      $latestProjects = Project::latest()->take(3)->get();
+      return Inertia::render('Dashboard', [
+        'projects' => $latestProjects,
+      ]);
+    } else if ($user->hasRole('client')) {
+      return Inertia::render('DashboardClient', []);
+    } else {
+      return Inertia::render('DashboardEmployee', []);
     }
+  }
 }
---
+++ resources/js/pages/DashboardClient.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Heading from '@/components/Heading.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+// defineProps<{ projects: projectsEntity[] }>();
+const breadcrumbs: BreadcrumbItem[] = [{ title: 'Dashboard', href: '/dashboard' }];
+</script>
+
+<template>
+  <Head title="Client Dashboard" />
+
+  <AppLayout :breadcrumbs="breadcrumbs">
+    <div class="flex flex-col gap-4 p-8">
+      <Heading title="Client Dashboard" description="These are your latest projects" />
+      <div class="flex flex-col gap-2">
+
+      </div>
+    </div>
+  </AppLayout>
+</template>

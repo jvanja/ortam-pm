87235d3c2de5f8cae0b51b34f71c26d66284811d
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-04-02 12:52:50 +0200

    removed button

--- resources/js/pages/projects/Index.vue
+++ resources/js/pages/projects/Index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import Heading from '@/components/Heading.vue';
-import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import ObjectList from '@/components/ObjectList.vue';

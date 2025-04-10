2414b0a11e6687553875232412b3a64f6087c4d0
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-04-10 14:24:57 +0200

    added another chart

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -28,7 +28,87 @@ const chartOptions = (project: Project) => {
     labels: [project.type],
   };
 };
-const series = () => [70];
+const getSeries = (project: Project) => {
+  console.log(project);
+  return [70];
+};
+
+const options = {
+  series: [
+    {
+      data: [
+        {
+          x: 'Analysis',
+          y: [new Date('2019-02-27').getTime(), new Date('2019-03-04').getTime()],
+          fillColor: '#008FFB',
+        },
+        {
+          x: 'Design',
+          y: [new Date('2019-03-04').getTime(), new Date('2019-03-08').getTime()],
+          fillColor: '#00E396',
+        },
+        {
+          x: 'Coding',
+          y: [new Date('2019-03-07').getTime(), new Date('2019-03-10').getTime()],
+          fillColor: '#775DD0',
+        },
+        {
+          x: 'Testing',
+          y: [new Date('2019-03-08').getTime(), new Date('2019-03-12').getTime()],
+          fillColor: '#FEB019',
+        },
+        {
+          x: 'Deployment',
+          y: [new Date('2019-03-12').getTime(), new Date('2019-03-17').getTime()],
+          fillColor: '#FF4560',
+        },
+      ],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: 'rangeBar',
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      distributed: true,
+      dataLabels: {
+        hideOverflowingLabels: false,
+      },
+    },
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val, opts) {
+      console.log(val)
+      const label = opts.w.globals.labels[opts.dataPointIndex];
+      // var a = moment(val[0]);
+      // var b = moment(val[1]);
+      //
+      const a = new Date(val[0])
+      const b = new Date(val[1])
+      // const diff = b.diff(a, 'days');
+      const diff = (val[1] - val[0]) / (1000 * 3600 * 24);
+      return label + ': ' + diff + (diff > 1 ? ' days' : ' day');
+    },
+    style: {
+      colors: ['#f3f4f5', '#fff'],
+    },
+  },
+  xaxis: {
+    type: 'datetime',
+  },
+  yaxis: {
+    show: false,
+  },
+  grid: {
+    row: {
+      colors: ['#f3f4f5', '#fff'],
+      opacity: 1,
+    },
+  },
+};
 </script>
 
 <template>
@@ -37,12 +117,19 @@ const series = () => [70];
   <AppLayout :breadcrumbs="breadcrumbs">
     <div v-if="projects.length > 1" class="flex flex-col gap-4 p-8">
       <Heading title="Projects" description="These are your latest projects" />
-      <div class="grid grid-cols-3 gap-2">
-        <div v-for="project in projects" :key="project.id" class="flex flex-col justify-between rounded-lg bg-neutral-100 px-1 py-4 dark:bg-neutral-800">
           <div class="chart-container">
-            <apexchart :options="chartOptions(project)" :series="series()"></apexchart>
+            <apexchart :options="options" :series="options.series"></apexchart>
           </div>
-          <div class="flex gap-2 justify-center">
+      <div class="grid grid-cols-3 gap-2">
+        <div
+          v-for="project in projects"
+          :key="project.id"
+          class="flex flex-col justify-between rounded-lg bg-neutral-100 px-1 py-4 dark:bg-neutral-800"
+        >
+          <div class="chart-container">
+            <apexchart :options="chartOptions(project)" :series="getSeries(project)"></apexchart>
+          </div>
+          <div class="flex justify-center gap-2">
             <Button variant="default"><a :href="`/projects/${project.id}`">Edit</a></Button>
             <Button variant="destructive">Delete</Button>
           </div>

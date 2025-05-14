59ee77a9a7dd9496200aca5f7975018d45bd9aa4
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-05-14 10:19:08 +0200

    added nullable fields to the migrations clients

--- database/migrations/2025_03_11_140000_create_clients_table.php
+++ database/migrations/2025_03_11_140000_create_clients_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('company_name');
             $table->string('contact_person');
-            $table->string('address');
-            $table->string('phone');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email');
             $table->foreignId('organization_id')->constrained()->onUpdate('cascade');
             $table->timestamps();

1942538f5d6af66311f309c804975f0089a8e8de
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-05-16 10:15:49 +0200

    added new invoice controller method

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -22,7 +22,7 @@ class InvoiceController extends Controller {
    * Show the form for creating a new resource.
    */
   public function create() {
-    //
+    return Inertia::render('invoices/Add');
   }
 
   /**
---
+++ resources/js/pages/invoices/Add.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import type { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [{ title: 'Invoices', href: '/invoices' }];
+</script>
+<template>
+  <Head title="Invoices" />
+
+  <AppLayout :breadcrumbs="breadcrumbs">
+    <div class="p-8">
+      New Invoice
+    </div>
+  </AppLayout>
+</template>

2e7b79e99216023239fb42e934740c22c18ceef0
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-05-26 15:55:00 +0200

    removed form from proejct files

--- resources/js/components/project/ProjectFiles.vue
+++ resources/js/components/project/ProjectFiles.vue
@@ -3,7 +3,6 @@ import Dropzone from '@/components/dropzone/Dropzone.vue';
 import FilesList from '@/components/dropzone/FilesList.vue';
 import { UploadService, message, progress, uploading } from '@/services/UploadService';
 import { ProjectFile, Project } from '@/types';
-import { useForm } from '@inertiajs/vue3';
 
 const props = defineProps<{
   project: Project & {
@@ -12,10 +11,6 @@ const props = defineProps<{
 }>();
 
 console.log(props.project.files);
-const form = useForm({
-  name: null,
-  avatar: null,
-});
 
 function upload(idx: number, file: File) {
   // @ts-expect-error It does
@@ -25,8 +20,6 @@ function upload(idx: number, file: File) {
 }
 
 function submit(selectedFiles: File[]) {
-  // form.post('/files/project/123/store')
-  console.log(selectedFiles);
   for (let i = 0; i < selectedFiles.length; i++) {
     upload(i, selectedFiles[i]);
   }
@@ -37,7 +30,9 @@ const errorMessage = '';
 <template>
   <Dropzone :errorMessage="errorMessage" @submit:files="submit" />
   <FilesList v-if="project.files.length > 0" :filesData="project.files" />
+  <div v-if="uploading">
   Progress: {{ progress }} <br />
   Message: {{ message }} <br />
   Uploading: {{ uploading }} <br />
+  </div>
 </template>

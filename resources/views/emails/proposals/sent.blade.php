234c0cb9b2cc60b58d6c2a281063ec120c1990a9
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-06-09 16:55:30 +0200

    changed to markdown

--- resources/views/emails/proposals/sent.blade.php
+++ resources/views/emails/proposals/sent.blade.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<html>
+@component('mail::message')
+Hello {{ $clientName }},
 
-<head>
-  <title>Your Proposal</title>
-</head>
+We are pleased to send you our proposal titled: **{{ $proposalTitle }}**.
 
-<body>
-  <h1>Hello {{ $clientName }},</h1>
-  <p>We are pleased to send you our proposal titled: <strong>{{ $proposalTitle }}</strong>.</p>
-  <p>You can view the proposal by clicking on the link below:</p>
-  <p><a href="{{ $proposalUrl }}">View Proposal</a></p>
-  <p>If you have any questions, please do not hesitate to contact us.</p>
-  <p>Thank you,<br>{{ config('app.name') }} Team</p>
-</body>
+You can view the proposal by clicking on the link below:
+@component('mail::button', ['url' => $proposalUrl])
+  View Proposal
+@endcomponent
 
-</html>
+If you have any questions, please do not hesitate to contact us.
+
+Thank you,
+
+{{ config('app.name') }} Team
+@endcomponent

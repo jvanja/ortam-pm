08faa6ce36ad4070cfdebaa42ce6f5a1f47bd81f
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-06-10 10:34:56 +0200

    fixed tasks styles

--- resources/js/components/project/PipelineStageTasks.vue
+++ resources/js/components/project/PipelineStageTasks.vue
@@ -15,25 +15,27 @@ const addTask = () => {
   <Table class="min-w-full divide-y divide-gray-200">
     <TableHeader>
       <TableRow class="text-gray-500">
-        <TableHead class="border-b p-2 text-left text-xs font-normal"><Checkbox /></TableHead>
-        <TableHead class="border-b p-2 text-left text-xs font-normal">Task</TableHead>
-        <TableHead class="border-b p-2 text-left text-xs font-normal">User</TableHead>
+        <TableHead class="border-b p-2 text-left text-xs font-normal text-left w-8">
+          <Checkbox />
+        </TableHead>
+        <TableHead class="border-b p-2 text-left text-xs font-normal text-left">Task</TableHead>
+        <TableHead class="border-b p-2 text-left text-xs font-normal text-right">User</TableHead>
       </TableRow>
     </TableHeader>
     <TableBody>
       <TableRow v-for="(task, index) in tasks" :key="index">
         <TableCell class="px-2 py-4 text-sm">
-          <Checkbox v-model="task.id" />
+          <Checkbox v-model="task.id" class="size-4"/>
         </TableCell>
         <TableCell class="px-2 py-4 text-sm">
           {{ task.task_performed }}
         </TableCell>
-        <TableCell class="px-2 py-4 text-sm">
+        <TableCell class="px-2 py-4 text-sm text-right">
           {{ task.user_id }}
         </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell colspan="2" class="px-2 py-4">
+        <TableCell colspan="3" class="px-2 py-4">
           <div class="rounded-lg border-2 border-dashed border-teal-400 bg-teal-50 p-1 px-4 py-2 dark:bg-neutral-800">
             <button type="button" @click="addTask" class="flex w-full flex-1 items-center gap-2 text-sm text-green-700">
               <PlusCircle class="text-green-700" />

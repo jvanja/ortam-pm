e14daecaec69fc8069ad11e1cfe00a23a5fe63ea
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-06-19 11:25:51 +0200

    added ogr logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -6,11 +6,14 @@ import type { Organization } from '@/types';
 
 const page = usePage()
 const organization = computed(() => page.props.organization as Organization);
+const organizationLogo = computed(() => organization.value?.logo)
 </script>
 
 <template>
   <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-    <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+
+    <img v-if="organizationLogo" :src="organizationLogo" class="h-full w-full object-cover" />
+    <AppLogoIcon v-else class="size-5 fill-current text-white dark:text-black" />
   </div>
   <div class="ml-1 grid flex-1 text-left text-sm">
     <span v-if="organization" class="mb-0.5 truncate font-semibold leading-none">{{ organization.name }}</span>
